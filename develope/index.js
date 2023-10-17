40144ea34144c720d5e23d4351c4fc8bceffcf1e
chandler ames <eddiehawk1991@gmail.com>
2023-10-17 00:27:41 -0700

    index.js view database

--- develope/index.js
+++ develope/index.js
@@ -1,1 +1,44 @@
+const mysql = require('mysql2');
+const consoleTable = require('console.table');
+
+const connection = mysql.createConnection({
+  host: 'your_db_host',
+  user: 'root',
+  password: 'atari1991',
+  database: 'store_db',
+});
+
+
+function viewDepartments() {
+  connection.query('SELECT * FROM department', (err, results) => {
+    if (err) throw err;
+    console.table(results);
+  });
+}
+
+
+function viewRoles() {
+  connection.query('SELECT * FROM role', (err, results) => {
+    if (err) throw err;
+    console.table(results);
+  });
+}
+
+
+function viewEmployees() {
+  connection.query('SELECT * FROM employee', (err, results) => {
+    if (err) throw err;
+    console.table(results);
+  });
+}
+
+
+connection.connect((err) => {
+  if (err) throw err;
+
+  
+  viewDepartments();
+  viewRoles();
+  viewEmployees();
+});
 
